87e4440130ed31e3380b41a5b48a189dccb34c77
Pieter Maene <pieter@maene.eu>
2021-10-31 18:49:51 +0100

    Set up basic CLI structure and prepared collector

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"github.com/pmaene/smartpvms_exporter/cmd"
+)
+
+var (
+	version = ""
+	commit  = ""
+)
+
 func main() {
+	cmd.Execute()
+}
+
+func init() {
+	cmd.SetVersion(version)
+	cmd.SetCommit(commit)
 }
